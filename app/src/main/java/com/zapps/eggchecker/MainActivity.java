b0929254d8c03bece7e858faba22e9f198a6b94a
지민 <wlals822@naver.com>
2017-08-18 21:54:38 +0900

    webview 만들고 javascript 로 검색

--- app/src/main/java/com/zapps/eggchecker/MainActivity.java
+++ app/src/main/java/com/zapps/eggchecker/MainActivity.java
@@ -1,13 +1,101 @@
 package com.zapps.eggchecker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView webView;
+
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        button = (Button) findViewById(R.id.button);
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new MyJavaScriptInterface(this), "HtmlViewer");
+
+        final Activity activity = this;
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+            }
+
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d("weblog", consoleMessage.message() + '\n' + consoleMessage.messageLevel() +
+                        '\n' + consoleMessage.sourceId());
+                return super.onConsoleMessage(consoleMessage);
+            }
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        webView.loadUrl("http://www.foodsafetykorea.go.kr/portal/fooddanger/eggHazardList.do?menu_grp=MENU_NEW02&menu_no=3497");
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    webView.evaluateJavascript("javascript:"
+                                    + "document.getElementById(\"search_keyword\").value=\"08LSH\"",
+                            new ValueCallback<String>() {
+                                @Override
+                                public void onReceiveValue(String value) {
+                                    Log.d("onrecieve", value);
+                                }
+                            });
+
+                    webView.evaluateJavascript(
+                        "(function() { return ('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>'); })();",
+                        new ValueCallback<String>() {
+                            @Override
+                            public void onReceiveValue(String html) {
+                                Log.d("HTML", html);
+                                // code here
+                            }
+                        });
+                    webView.evaluateJavascript("javascript: searchList()",null);
+            }
+        });
+
+
+    }
+
+    class MyJavaScriptInterface {
+        private Context context;
+
+        public MyJavaScriptInterface(Context context) {
+            this.context = context;
+        }
+
+        public String  getHtmlFromResult(String html) {
+            Log.d("html",html);
+            return html;
+        }
     }
 }
